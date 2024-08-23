0db7fc042590fef4fffd5512af20158c5d9e7985
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-08-23 17:20:03 +0100

    added event listeners for all buttons and added for loop with switch for mathematical operators

--- src/main.ts
+++ src/main.ts
@@ -14,19 +14,19 @@ const registerClick = (event: Event) => {
     const target = event.currentTarget as HTMLButtonElement;
     const value = target.innerText;
 
-    // 'AC' button (clear display)
+    // 'AC' button (clear display):
     if (value === 'AC') {
         display.value = '';
         return;
     }
 
-    // 'DEL' button (delete last character)
+    // 'DEL' button (delete last character):
     if (value === 'DEL') {
         display.value = display.value.slice(0, -1);
         return;
     }
 
-    // '=' button
+    // '=' button:
     if (value === '=') {
         try {
             const result = evaluateExpression(display.value);
@@ -36,4 +36,49 @@ const registerClick = (event: Event) => {
         }
         return;
     }
+};
+
+
+// Event listeners for all buttons:
+buttons.forEach((button) => button.addEventListener("click", registerClick));
+
+const evaluateExpression = (expression: string): number => {
+    const numbers: number[] = [];
+    const ops: string[] = [];
+    let currentNumber = '';
+
+    // Iterate through each character:
+    for (const char of expression) {
+        if (char >= '0' && char <= '9' || char === '.') {
+            // Append digit or decimal point to the current number
+            currentNumber += char;
+        } else if (char === '+' || char === '-' || char === '*' || char === '/') {
+            ops.push(char);
+        }
+    }
+
+    // Evaluate mathenatical expression depending on operator:
+    let result = numbers[0];
+    for (let i = 0; i < ops.length; i++) {
+        const operator = ops[i];
+        const num = numbers[i + 1];
+        switch (operator) {
+            case "+":
+                result += num;
+                break;
+            case "-":
+                result -= num;
+                break;
+            case "*":
+                result *= num;
+                break;
+            case "/":
+                result /= num;
+                break;
+            default:
+                throw new Error('Not a valid operation');
+        }
+    }
+
+    return result;
 };
