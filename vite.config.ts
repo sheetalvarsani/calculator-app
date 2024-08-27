341c47b5f36c20e8b3aab25a248cf1b90371d4c2
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-08-27 10:37:11 +0100

    added vite.config.ts file

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/calculator-app/', // Replace <REPO> with your repository name on GitHub
+});
